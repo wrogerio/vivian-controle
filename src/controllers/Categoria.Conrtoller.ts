954f8812987dbcc3832fc7ffc14d2acdf4fd1998
wellington <wrogeriowrs@gmail.com>
2023-02-22 00:27:24 -0300

    .

--- src/controllers/Categoria.Conrtoller.ts
+++ src/controllers/Categoria.Conrtoller.ts
@@ -3,8 +3,8 @@ import { PrismaClient } from "@prisma/client";
 export const getCategorias = async () => {
     const prisma = new PrismaClient();
     try {
-        const categorias = await prisma.categorias.findMany();
-        return categorias;
+        const categ = await prisma.categorias.findMany();
+        return categ;
     } catch (error) {
         console.log(error);
     }
