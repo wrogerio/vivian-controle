3b4709e6c2be04893c72c2be143de8456c577426
wellington <wrogeriowrs@gmail.com>
2023-02-22 12:57:51 -0300

    typeOrm

--- src/controllers/Categoria.Conrtoller.ts
+++ src/controllers/Categoria.Conrtoller.ts
@@ -1,11 +1,3 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
-
 export const getCategorias = async () => {
-    const categ = await prisma.categorias.findMany({
-        include: {
-            Lancamentos: false,
-        },
-    });
-    return categ;
+    return null;
 };
