57080952d2bed8aa8ce2c024c8287c1997887790
wellington <wrogeriowrs@gmail.com>
2023-02-22 00:12:11 -0300

    .

--- src/pages/api/categorias/index.ts
+++ src/pages/api/categorias/index.ts
@@ -5,6 +5,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     switch (req.method) {
         case "GET":
             const categ = await getCategorias();
+            console.log(categ);
             res.status(200).json(categ);
             break;
         case "POST":
--- src/pages/categorias/index.tsx
+++ src/pages/categorias/index.tsx
@@ -14,6 +14,7 @@ const Index = () => {
     const LoadData = async () => {
         const res = await fetch(`/api/${urlRoot}`);
         const data = await res.json();
+        console.log(data);
 
         // error handle
         if (data.error) console.log(data.error);
