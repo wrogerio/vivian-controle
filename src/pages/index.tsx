b23f2e9721b26217a9a1b82d94fb44d5c6148aa3
wellington <wrogeriowrs@gmail.com>
2023-02-23 12:33:03 -0300

    api e controllers

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,32 @@
 export default function Home() {
+    const dtNow = new Date();
+
+    const getNow = () => {
+        const dtNow = new Date();
+
+        // return yyyy-MM
+        return dtNow.toISOString().substr(0, 7);
+    };
+
     return (
         <>
-            <h1>Hello World dsad</h1>
+            <div className="row border-bottom mb-1">
+                <div className="col text-center pt-2">
+                    <h3 className="text-danger">Dashboard</h3>
+                    <h2>R$ 2.450,36</h2>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col">
+                    <div className="p-2 text-center ">
+                        <div className="form-group">
+                            <label htmlFor="mes">Selecione Mës/Ano</label>
+                            <input type="month" name="mes" className="form-control" style={{ textAlign: "center" }} defaultValue={getNow()} />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
