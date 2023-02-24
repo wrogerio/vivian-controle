3a64413badd741b2a4fd0f2b0b20da7b0a0e7d54
wellington <wrogeriowrs@gmail.com>
2023-02-24 13:32:38 -0300

    api e controllers

--- src/controllers/Categoria.Conrtoller.ts
+++ src/controllers/Categoria.Conrtoller.ts
@@ -2,70 +2,62 @@ import pool from "@/database/dbSQL";
 import db from "../database/db";
 import { categoria } from "./../interfaces/index";
 
-export const GetAllSQLServer = () => {
-    return new Promise(async (resolve, reject) => {
-        await pool.connect();
-        const result = await pool.request().query("SELECT * FROM categorias Order By Nome");
-        resolve(result.recordset);
-    });
-};
-
 export const GetAll = () => {
-    return new Promise((resolve, reject) => {
-        db.query("SELECT * FROM categorias Order By Nome", (err, result) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(result);
-            }
-        });
+    return new Promise(async (resolve, reject) => {
+        try {
+            await pool.connect();
+            const result = await pool.request().query("SELECT * FROM categorias Order By Nome");
+            resolve(result.recordset);
+        } catch (err) {
+            reject(err);
+        }
     });
 };
 
 export const GetById = (id: number) => {
-    return new Promise((resolve, reject) => {
-        db.query("SELECT * FROM categorias WHERE Id = ?", [id], (err, result) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(result);
-            }
-        });
+    return new Promise(async (resolve, reject) => {
+        try {
+            await pool.connect();
+            const result = await pool.request().query("SELECT * FROM categorias WHERE Id = " + id);
+            resolve(result.recordset);
+        } catch (err) {
+            reject(err);
+        }
     });
 };
 
 export const Add = (categoria: categoria) => {
-    return new Promise((resolve, reject) => {
-        db.query("INSERT INTO categorias SET ?", [categoria], (err, result) => {
-            if (err) {
-                reject(false);
-            } else {
-                resolve(true);
-            }
-        });
+    return new Promise(async (resolve, reject) => {
+        try {
+            await pool.connect();
+            const result = await pool.request().query("INSERT INTO categorias (Nome) VALUES ('" + categoria.Nome + "')");
+            resolve(result.recordset);
+        } catch (err) {
+            reject(err);
+        }
     });
 };
 
 export const Update = (categoria: categoria) => {
-    return new Promise((resolve, reject) => {
-        db.query("UPDATE categorias SET ? WHERE Id = ?", [categoria, categoria.Id], (err, result) => {
-            if (err) {
-                reject(false);
-            } else {
-                resolve(true);
-            }
-        });
+    return new Promise(async (resolve, reject) => {
+        try {
+            await pool.connect();
+            const result = await pool.request().query("UPDATE categorias SET Nome = '" + categoria.Nome + "' WHERE Id = " + categoria.Id);
+            resolve(result.recordset);
+        } catch (err) {
+            reject(err);
+        }
     });
 };
 
 export const Delete = (id: number) => {
-    return new Promise((resolve, reject) => {
-        db.query("DELETE FROM categorias WHERE Id = ?", [id], (err, result) => {
-            if (err) {
-                reject(false);
-            } else {
-                resolve(true);
-            }
-        });
+    return new Promise(async (resolve, reject) => {
+        try {
+            await pool.connect();
+            const result = await pool.request().query("DELETE FROM categorias WHERE Id = " + id);
+            resolve(result.recordset);
+        } catch (err) {
+            reject(err);
+        }
     });
 };
