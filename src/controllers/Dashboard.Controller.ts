800aff9d2f76f5e88caade388bc77b2664687bc1
wellington <wrogeriowrs@gmail.com>
2023-03-05 12:19:25 -0300

    .

--- src/controllers/Dashboard.Controller.ts
+++ src/controllers/Dashboard.Controller.ts
@@ -39,7 +39,7 @@ export const GetItems = async (ano: number, mes: number) => {
     var querie = `  SELECT c.Nome AS Categoria, SUM(l.Valor) AS Total
                     FROM Lancamentos l
                     INNER JOIN Categorias c ON l.CategoriaId = c.Id
-                    WHERE YEAR(l.DtLancamento) = ${ano} AND MONTH(l.DtLancamento) = ${mes}
+                    WHERE YEAR(l.DtLancamento) = ${ano} AND MONTH(l.DtLancamento) = ${mes} And l.TipoId = '1BAEA2BD-5E43-41ED-926D-FFE0DB46EB6B'
                     GROUP BY c.Nome
                     ORDER BY 2 DESC`;
     return new Promise(async (resolve, reject) => {
